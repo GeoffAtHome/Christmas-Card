c136cf0ba96ef6b284f3b1a8fecae3351d19b0f1
GeoffAtHome <geoff@soord.org.uk>
2024-02-27 19:48:46 +0000

    Better still

--- src/components/data-image.ts
+++ src/components/data-image.ts
@@ -13,7 +13,5 @@ export function imageLoaded(e: any /* HTMLImageElement */) {
   if (large !== null) {
     e.target.src = large;
     e.target.removeAttribute('large');
-  } else {
-    e.target.removeAttribute('style');
   }
 }
